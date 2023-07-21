7058d2f98cc0dd3539b410a4f17ccfa12136ba03
Łukasz Wojczuk <91937092+Catkat11@users.noreply.github.com>
2023-07-21 12:25:22 +0200

    Add files via upload

---
+++ flights_deal_finder/notification_manager.py
@@ -0,0 +1,20 @@
+from twilio.rest import Client
+
+TWILIO_SID = "SID"
+TWILIO_AUTH_TOKEN = "AUTH_TOKEN"
+TWILIO_VIRTUAL_NUMBER = "VIR_NUMB"
+TWILIO_VERIFIED_NUMBER = "VER_NUMB"
+
+
+class NotificationManager:
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER
+        )
+        print(message.sid)
+        
